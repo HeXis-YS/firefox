849b265f22254779cf9c6f03c5553904b5dabb1c
HeXis-YS <40174982+HeXis-YS@users.noreply.github.com>
2025-10-22 21:20:45 +0800

    JetStream main 3db87bec0d0b424f3f8150390774cbf29244a57c

---
+++ third_party/webkit/PerformanceTests/JetStream/generators/async-file-system.js
@@ -0,0 +1,212 @@
+/*
+ * Copyright (C) 2018-2023 Apple Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+"use strict";
+
+function computeIsLittleEndian() {
+    let buf = new ArrayBuffer(4);
+    let dv = new DataView(buf);
+    dv.setUint32(0, 0x11223344, true);
+    let view = new Uint8Array(buf);
+    return view[0] === 0x44;
+}
+
+const isLittleEndian = computeIsLittleEndian();
+let globalCounter = 0;
+
+function randomFileContents() {
+    const numBytes = (globalCounter % 128)  + 2056;
+    globalCounter++;
+    let result = new ArrayBuffer(numBytes);
+    let view = new Uint8Array(result);
+    for (let i = 0; i < numBytes; ++i)
+        view[i] = (i + globalCounter) % 255;
+    return new DataView(result);
+}
+
+class File {
+    constructor(dataView, permissions) {
+        this._data = dataView;
+    }
+
+    get data() { return this._data; }
+
+    set data(dataView) { this._data = dataView; }
+
+    swapByteOrder() {
+        let hash = 0x1a2b3c4d;
+        for (let i = 0; i < Math.floor(this.data.byteLength / 8) * 8; i += 8) {
+            const data = this.data.getFloat64(i, isLittleEndian);
+            this.data.setFloat64(i, data, !isLittleEndian);
+            hash ^= data | 0;
+        }
+        return hash;
+    }
+}
+
+class Directory {
+    constructor() {
+        this.structure = new Map;
+    }
+
+    async addFile(name, file) {
+        let entry = this.structure.get(name);
+        if (entry !== undefined) {
+            if (entry instanceof File)
+                throw new Error("Can't replace file with file.");
+            if (entry instanceof Directory)
+                throw new Error("Can't replace a file with a new directory.");
+            throw new Error("Should not reach this code");
+        }
+
+        this.structure.set(name, file);
+        return file;
+    }
+
+    async addDirectory(name, directory = new Directory) {
+        let entry = this.structure.get(name);
+        if (entry !== undefined) {
+            if (entry instanceof File)
+                throw new Error("Can't replace file with directory.");
+            if (entry instanceof Directory)
+                throw new Error("Can't replace directory with new directory.");
+            throw new Error("Should not reach this code");
+        }
+
+        this.structure.set(name, directory);
+        return directory;
+    }
+
+    async* ls() {
+        for (let [name, entry] of this.structure)
+            yield { name, entry, isDirectory: entry instanceof Directory };
+    }
+
+    async* forEachFile() {
+        for await (let item of this.ls()) {
+            if (!item.isDirectory)
+                yield item;
+        }
+    }
+
+    async* forEachFileRecursively() {
+        for await (let item of this.ls()) {
+            if (item.isDirectory) {
+                for await (let file of item.entry.forEachFileRecursively())
+                    yield file;
+            } else {
+                yield item;
+            }
+        }
+    }
+
+    async* forEachDirectoryRecursively() {
+        for await (let item of this.ls()) {
+            if (!item.isDirectory)
+                continue;
+
+            for await (let dirItem of item.entry.forEachDirectoryRecursively())
+                yield dirItem;
+
+            yield item;
+        }
+    }
+
+    async fileCount() {
+        let count = 0;
+        for await (let item of this.ls()) {
+            if (!item.isDirectory)
+                ++count;
+        }
+
+        return count;
+    }
+
+    async rm(name) {
+        return this.structure.delete(name);
+    }
+}
+
+async function setupDirectory() {
+    const fs = new Directory;
+    let dirs = [fs];
+    let counter = 0;
+    for (let dir of dirs) {
+        for (let i = 0; i < 10; ++i) {
+            if (dirs.length < 400 && (counter % 3) <= 1) {
+                dirs.push(await dir.addDirectory(`dir-${i}`));
+            }
+            counter++;
+        }
+    }
+
+    for (let dir of dirs) {
+        for (let i = 0; i < 5; ++i) {
+            if ((counter % 3) === 0) {
+                await dir.addFile(`file-${i}`, new File(randomFileContents()));
+            }
+            counter++;
+        }
+    }
+
+    return fs;
+}
+
+class Benchmark {
+    EXPECTED_FILE_COUNT = 666;
+
+    totalFileCount = 0;
+    lastFileHash = undefined;
+
+    async runIteration() {
+        const fs = await setupDirectory();
+
+        for await (let { entry: file } of fs.forEachFileRecursively()) {
+            this.lastFileHash = file.swapByteOrder();
+        }
+
+        for await (let { name, entry: dir } of fs.forEachDirectoryRecursively()) {
+            if ((await dir.fileCount()) > 3) {
+                for await (let { name } of dir.forEachFile()) {
+                    let result = await dir.rm(name);
+                    if (!result)
+                        throw new Error("rm should have returned true");
+                }
+            }
+        }
+
+        for await (let _ of fs.forEachFileRecursively()) {
+            this.totalFileCount++;
+        }
+    }
+
+    validate(iterations) {
+        const expectedFileCount = this.EXPECTED_FILE_COUNT * iterations;
+        if (this.totalFileCount != expectedFileCount)
+            throw new Error(`Invalid total file count ${this.totalFileCount}, expected ${expectedFileCount}.`);
+        if (this.lastFileHash === undefined)
+            throw new Error(`Invalid file hash: ${this.lastFileHash}`);
+    }
+}
---
+++ third_party/webkit/PerformanceTests/JetStream/tests/run-shell.mjs
@@ -0,0 +1,162 @@
+#! /usr/bin/env node
+
+import commandLineArgs from "command-line-args";
+import { spawn } from  "child_process";
+import { fileURLToPath } from "url";
+import { styleText } from "node:util";
+import * as path from "path";
+import * as fs from "fs";
+import * as os from "os";
+import core from "@actions/core";
+
+import {logInfo, logError, logGroup, printHelp, runTest, GITHUB_ACTIONS_OUTPUT} from "./helper.mjs";
+
+const optionDefinitions = [
+  { name: "shell", type: String, description: "Set the shell to test, choices are [jsc, v8, spidermonkey]." },
+  { name: "help", alias: "h", description: "Print this help text." },
+];
+
+const options = commandLineArgs(optionDefinitions);
+
+if ("help" in options)
+  printHelp(optionDefinitions);
+
+const JS_SHELL= options?.shell;
+if (!JS_SHELL)
+  printHelp("No javascript shell specified, use --shell", optionDefinitions);
+
+const SHELL_NAME = (function() {
+  switch (JS_SHELL) {
+      case "javascriptcore":
+      case "jsc": {
+          return "javascriptcore";
+    }
+      case "spidermonkey": {
+          return "spidermonkey";
+      }
+      case "v8": {
+          return "v8";
+      }
+      default: {
+          printHelp(`Invalid shell "${JS_SHELL}", choices are: "jsc", "spidermonkey" and "v8)`);
+      }
+  }
+})();
+
+const FILE_PATH = fileURLToPath(import.meta.url);
+const SRC_DIR = path.dirname(path.dirname(FILE_PATH));
+const CLI_PATH = path.join(SRC_DIR, "cli.js");
+const UNIT_TEST_PATH = path.join(SRC_DIR, "tests", "unit-tests.js");
+
+function convertCliArgs(cli, ...cliArgs) {
+  if (SHELL_NAME == "spidermonkey")
+    return [cli, ...cliArgs];
+  return [cli, "--", ...cliArgs];
+}
+
+
+const SPAWN_OPTIONS =  { 
+  stdio: ["inherit", "inherit", "inherit"]
+};
+
+async function sh(binary, ...args) {
+  const cmd = `${binary} ${args.join(" ")}`;
+  if (GITHUB_ACTIONS_OUTPUT) {
+    core.startGroup(binary);
+    core.notice(styleText("blue", cmd));
+  } else {
+    console.log(styleText("blue", cmd));
+  }
+  try {
+    const result = await spawnCaptureStdout(binary, args, SPAWN_OPTIONS);
+    if (result.status || result.error) {
+      logError(result.error);
+      throw new Error(`Shell CMD failed: ${binary} ${args.join(" ")}`);
+    }
+    return result;
+  } finally {
+    if (GITHUB_ACTIONS_OUTPUT)
+      core.endGroup();
+  }
+}
+
+async function spawnCaptureStdout(binary, args) {
+  const childProcess = spawn(binary, args);
+  childProcess.stdout.pipe(process.stdout);
+  return new Promise((resolve, reject) => {
+    childProcess.stdoutString = "";
+    childProcess.stdio[1].on("data", (data) => {
+      childProcess.stdoutString += data.toString();
+    });
+    childProcess.on('close', (code) => {
+      if (code === 0) {
+        resolve(childProcess);
+      } else {
+        // Reject the Promise with an Error on failure
+        const error = new Error(`Command failed with exit code ${code}: ${binary} ${args.join(" ")}`);
+        error.process = childProcess;
+        error.stdout = childProcess.stdoutString;
+        error.exitCode = code;
+        reject(error);
+      }
+    });
+    childProcess.on('error', reject);
+  })
+}
+
+async function runTests() {
+    const shellBinary = await logGroup(`Installing JavaScript Shell: ${SHELL_NAME}`, testSetup);
+    let success = true;
+    success &&= await runTest("Run UnitTests", () => sh(shellBinary, UNIT_TEST_PATH));
+    success &&= await runCLITest("Run Single Suite", shellBinary, "proxy-mobx");
+    success &&= await runCLITest("Run Tag No Prefetch", shellBinary, "proxy", "argon2-wasm", "--no-prefetch");
+    success &&= await runCLITest("Run Grouped with Details", shellBinary, "SunSpider", "--group-details");
+    success &&= await runCLITest("Run Disabled Suite", shellBinary, "disabled");
+    success &&= await runCLITest("Run Default Suite",  shellBinary);
+    if (!success)
+      process.exit(1);
+}
+
+function jsvuOSName() {
+  const osName = () => {
+      switch (os.platform()) {
+          case "win32": return "win";
+          case "darwin": return "mac";
+          case "linux": return "linux";
+          default: throw new Error("Unsupported OS");
+      }
+  };
+  const osArch = () => {
+      switch (os.arch()) {
+          case "x64": return "64";
+          case "arm64": return "64arm";
+          default: throw new Error("Unsupported architecture");
+      }
+  };
+  return `${osName()}${osArch()}`;
+}
+
+const DEFAULT_JSC_LOCATION = "/System/Library/Frameworks/JavaScriptCore.framework/Versions/Current/Helpers/jsc"
+
+async function testSetup() {
+    await sh("jsvu", `--engines=${SHELL_NAME}`, `--os=${jsvuOSName()}`);
+    let shellBinary = path.join(os.homedir(), ".jsvu/bin", SHELL_NAME);
+    if (!fs.existsSync(shellBinary) && SHELL_NAME == "javascriptcore")
+      shellBinary = DEFAULT_JSC_LOCATION;
+    if (!fs.existsSync(shellBinary))
+      throw new Error(`Could not find shell binary: ${shellBinary}`);
+    logInfo(`Installed JavaScript Shell: ${shellBinary}`);
+    return shellBinary;
+}
+
+function runCLITest(name, shellBinary, ...args) {
+  return runTest(name, () => runShell(shellBinary, ...convertCliArgs(CLI_PATH, ...args)));
+}
+
+async function runShell(shellBinary, ...args) {
+  const result = await sh(shellBinary, ...args);
+  if (result.stdoutString.includes("JetStream3 failed"))
+    throw new Error("test failed")
+}
+
+setImmediate(runTests);
